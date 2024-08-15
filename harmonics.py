9a4e842467c3005c98b9ebffdb47b57c9b75fa68
Alexey Snytnikov <snytav@gmail.com>
2024-08-16 00:36:06 +0200

    1st

---
+++ harmonics.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+
+from laplace2D import laplace2D
+
+def make_small_debug_file(fi2D,al2D,v2D):
+    fi2D_10  = fi2D[:10,:10]
+    al2D_10  = al2D[:10, :10]
+    v2D_10   = v2D[:10, :10]
+    val = np.zeros((100,3))
+    val[:,0] = fi2D_10.reshape(100)
+    val[:,1] = al2D_10.reshape(100)
+    val[:,2] = v2D_10.reshape(100)
+    return fi2D_10, al2D_10, v2D_10, True
+
+class HarmNet(nn.Module):
+    def __init__(self,N,fi,al,v,koef):
+        super(HarmNet,self).__init__()
+        self.fi = fi
+        self.al = al
+        self.v = v
+        self.koef = koef.reshape(koef.shape[0]*koef.shape[1])
+        self.N = koef.shape[0]
+
+        N_fi = np.unique(self.fi).shape[0]
+        N_al = np.unique(self.al).shape[0]
+        self.v2D = self.v.reshape(N_al, N_fi).numpy()
+        self.rhs = laplace2D(self.v2D)
+        self.fi2D = self.fi.reshape(N_al, N_fi)
+        self.al2D = self.al.reshape(N_al, N_fi)
+
+        # temporarily reduce size for debug purpose
+        self.fi2D, self.al2D, self.v2D, self.debug_mode = make_small_debug_file(self.fi2D, self.al2D, self.v2D)
+        fc1 = nn.Linear(2, self.koef.shape[0])
+        fc2 = nn.Linear(self.N, 1)
+        self.fc1 = fc1
+        self.fc2 = fc2
+    def forward(self,x):
+        x = self.fc1(x)
+        x = torch.sigmoid(x)
+        x = self.fc2(x)
+        return x
+
+    def train(self):
+
+        optim = torch.optim.Adam(self.parameters(),lr=0.01)
+
+        lf = torch.abs(self.rhs-self.forward(self.koef))
+
+        n = 0
+        while lf.item() > 0.01:
+            optim.zero_grad()
+            lf = torch.abs(self.rhs - self.forward(self.koef))
+            optim.step()
+            print(n,lf.item())
+            n = n+1
+
+if __name__ == '__main__':
+    crd = np.loadtxt('testgrid.grid')
+    N_fi = np.unique(crd[:, 0]).shape[0]
+    N_al = np.unique(crd[:,1]).shape[0]
+    df = df_user_key_word_org = pd.read_csv("new",
+                                   sep="\s+|;|:",
+                                   engine="python")
+    # net = GeoNet(10)
+    v = df['P_mod'].values
+    fi = df['fi'].values
+    lb = df['lb'].values
+    # t = np.concatenate((fi.reshape(fi.shape[0], 1), lb.reshape(fi.shape[0], 1)), axis=1)
+    # t = torch.from_numpy(t)
+    # vt = net.forward(t.float())
+    # optimizer = torch.optim.Adam(net.parameters(),lr = 0.01)
+    # lf = torch.ones(1)*1e9
+    v_torch = torch.from_numpy(v)
+    # v2D = v_torch.reshape(N_al,N_fi)
+
+    from read_harmonics import read_gfc
+    koef = read_gfc()
+
+    #from PDE import PDEnet
+    pde  = HarmNet(10,fi,lb,v_torch,koef)
+
+
+
+
+    qq = 0
---
+++ laplace2D.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+
+def laplace2D(pd):
+    d2x = pd[2:, 1:-1] + pd[:-2, 1:-1] - 2.0 * pd[1:-1, 1:-1]
+    d2y = pd[1:-1, 2:] + pd[1:-1, :-2] - 2.0 * pd[1:-1, 1:-1]
+
+
+    f_in = -(d2x+d2y)
+    f = np.zeros((f_in.shape[0]+2,f_in.shape[1]+2))
+    f[1:-1,1:-1] = f_in
+    f[1:-1, 0] = f[1:-1, 1]
+    f[0,1:-1]  = f[1,1:-1]
+    f[1:-1, -1] = f[1:-1, -2]
+    f[-1,1:-1] = f[-2,1:-1]
+
+    f_ext = np.zeros((f.shape[0]+2,f.shape[1]+2))
+    f_ext[1:-1,1:-1] = f
+
+    return f
+
+
+if __name__ == '__main__':
+    N = 5
+    x = np.linspace(0,1,N)
+    y = np.linspace(0,1,N)
+
+    #fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    X,Y = np.meshgrid(x,y)
+
+    k1 = 1.0
+    k2 = 1.0
+    f = k1*X+k2*Y
+    f = np.sin(2*np.pi*X)
+    # surf = ax.plot_surface(X, Y, f, cmap=cm.coolwarm,
+    #                        linewidth=0, antialiased=False)
+    # plt.show(block=True)
+
+    r = laplace2D(f)
+    plt.figure()
+    plt.plot(x, f[5, :], color='blue')
+    plt.plot(x[1:-1], r[5, :], color='red')
+    plt.show(block=True)
+
+    #    plt.show(block=True)
+    #plt.figure()
+    # surf = ax.plot_surface(X[1:-1,1:-1], Y[1:-1,1:-1], r, cmap=cm.coolwarm,
+    #                        linewidth=0, antialiased=False)
+    #plt.show(block=True)
+    qq = 0
